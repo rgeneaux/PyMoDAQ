9e49aa973d33a2e19a44165dcf640c805d744625
Sébastien Weber <seba.weber@gmail.com>
2024-12-04 21:42:31 +0100

    adding test for serialize DataToActuators

--- tests/utils/serializer_test.py
+++ tests/utils/serializer_test.py
@@ -17,3 +17,14 @@ def test_data_from_plugins_serialization_deserialization():
     dwa = data_mod.DataFromPlugins('myplug', data=[np.array([1, 2, 3])])
     dwa.create_missing_axes()
     assert ser_factory.get_apply_deserializer(ser_factory.get_apply_serializer(dwa)) == dwa
+
+
+def test_dta_serialization_deserialization():
+    dwa1 = data_mod.DataActuator(data=9.5)
+    dwa2 = data_mod.DataActuator(data=2.5)
+
+    dta = data_mod.DataToActuators('myacts', data=[dwa1, dwa2], mode='abs')
+    dta_back = ser_factory.get_apply_deserializer(ser_factory.get_apply_serializer(dta))
+    assert dta_back == dta
+
+    assert dta_back.mode == dta.mode
